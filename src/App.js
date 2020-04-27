87f152c798c5cbeb20a597044d04e8d4f20ca179
Shoaib Bhimani <shoaibbhimani@Shoaibs-MacBook-Pro.local>
2020-04-28 00:37:21 +0530

    use-state with form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPasssword] = useState("");
+  const [showForm, toggleForm] = useState(true);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log({
+      email: email,
+      password: password,
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => {
+            toggleForm(!showForm);
+          }}
         >
-          Learn React
-        </a>
+          Toggle Form
+        </button>
+        {showForm && (
+          <form onSubmit={onSubmit}>
+            <label>
+              <div>Email</div>
+              <input
+                type="email"
+                value={email}
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+              />
+            </label>
+            <label>
+              <div>Password</div>
+              <input
+                type="password"
+                value={password}
+                onChange={(event) => {
+                  setPasssword(event.target.value);
+                }}
+              />
+            </label>
+
+            <div>
+              <button type="submit">Signup</button>
+            </div>
+          </form>
+        )}
       </header>
     </div>
   );
