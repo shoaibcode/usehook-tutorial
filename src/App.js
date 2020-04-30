efcbca2bd7249bcce0e3558141ac94ec457a8d2a
Shoaib Bhimani <shoaibbhimani@Shoaibs-MacBook-Pro.local>
2020-05-01 01:41:48 +0530

    use effect intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// No Dependience => Render All State
+// Empty Dependecies => Render ONce
+// Dependecies => Dependecies Change
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+
+  // ComponentDidMount
+  useEffect(() => {
+    console.log("Count");
+
+    return () => {
+      console.log("return Count");
+    };
+  }, [count]);
+
+  useEffect(() => {
+    console.log("Count 1");
+
+    return () => {
+      console.log("return Count 1");
+    };
+  }, [count1]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <div>{count}</div>
+          <div
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          >
+            Add Count
+          </div>
+        </div>
+
+        <div>
+          <div>{count1}</div>
+          <div
+            onClick={() => {
+              setCount1(count1 + 1);
+            }}
+          >
+            Add Count 1
+          </div>
+        </div>
       </header>
     </div>
   );
