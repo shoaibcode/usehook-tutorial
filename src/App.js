acffc0e4ceed03de58c082ac5e38e43d6477f034
shoaibcode <shoaibtechguru@gmail.com>
2020-05-07 21:36:58 +0530

    counter complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
+
+const counterReducer = (state, action) => {
+  console.log("state", state);
+  console.log("Action ", action);
+
+  if (action.type === "ADD_COUNT") {
+    return {
+      count: state.count + 1,
+    };
+  } else if (action.type === "SUBTRACT_COUNT") {
+    return {
+      count: state.count - 1,
+    };
+  }
+
+  return state;
+};
 
 function App() {
+  // const [state, setState] = useState({ count: 0 })
+  // setState(false)
+  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => {
+            dispatch({ type: "ADD_COUNT" });
+          }}
         >
-          Learn React
-        </a>
+          Add
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "SUBTRACT_COUNT" });
+          }}
+        >
+          Dec
+        </button>
+        {state.count}
       </header>
     </div>
   );
