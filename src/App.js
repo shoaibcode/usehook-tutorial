f9472048eef8421e567685b5c1f969c858fdfcd2
shoaibcode <shoaibtechguru@gmail.com>
2020-05-07 21:58:51 +0530

    todolist-complete

--- src/App.js
+++ src/App.js
@@ -1,46 +1,92 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 import "./App.css";
 
-const counterReducer = (state, action) => {
-  console.log("state", state);
-  console.log("Action ", action);
+const ACTION_TYPES = {
+  ADD_TODO: "ADD_TODO",
+  REMOVE_TODO: "REMOVE_TODO",
+  TOGGLE_TODO: "TOGGLE_TODO",
+};
 
-  if (action.type === "ADD_COUNT") {
-    return {
-      count: state.count + 1,
-    };
-  } else if (action.type === "SUBTRACT_COUNT") {
-    return {
-      count: state.count - 1,
-    };
+const todolistReducer = (state, action) => {
+  if (action.type === ACTION_TYPES.ADD_TODO) {
+    return state.concat({
+      text: action.payload,
+      completed: false,
+    });
+  } else if (action.type === ACTION_TYPES.REMOVE_TODO) {
+    return state.filter((todo, todoIndex) => {
+      return todoIndex !== action.payload;
+    });
+  } else if (action.type === ACTION_TYPES.TOGGLE_TODO) {
+    return state.map((todo, todoIndex) => {
+      if (action.payload === todoIndex) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+
+      return todo;
+    });
   }
 
   return state;
 };
 
 function App() {
-  // const [state, setState] = useState({ count: 0 })
-  // setState(false)
-  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
+  const [todos, dispatch] = useReducer(todolistReducer, []);
 
+  const [text, setText] = useState("");
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    dispatch({
+      type: ACTION_TYPES.ADD_TODO,
+      payload: text,
+    });
+
+    setText("");
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <button
-          onClick={() => {
-            dispatch({ type: "ADD_COUNT" });
-          }}
-        >
-          Add
-        </button>
-        <button
-          onClick={() => {
-            dispatch({ type: "SUBTRACT_COUNT" });
-          }}
-        >
-          Dec
-        </button>
-        {state.count}
+        <form onSubmit={onSubmitHandler}>
+          <input
+            type="text"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+          />
+          <button type="submit">Add Todo</button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => {
+            return (
+              <li key={index} className={todo.completed ? "completed" : ""}>
+                <span
+                  onClick={() => {
+                    dispatch({
+                      type: ACTION_TYPES.TOGGLE_TODO,
+                      payload: index,
+                    });
+                  }}
+                >
+                  {todo.text}
+                </span>
+                <button
+                  onClick={() => {
+                    dispatch({
+                      type: ACTION_TYPES.REMOVE_TODO,
+                      payload: index,
+                    });
+                  }}
+                >
+                  Remove
+                </button>
+              </li>
+            );
+          })}
+        </ul>
       </header>
     </div>
   );
